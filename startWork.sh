9f58984e8ce01853bdf707327e9c6bdf9afd96b6
André Torres <andregtorres@gmail.com>
2018-07-29 12:47:15 +0100

    change path

--- startWork.sh
+++ startWork.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # lauch atom nautilus and terminal on a location
 # 27-7-2018
-cd /home/agtorres/Data/Dropbox/IST/tokamakstraining
+cd /home/agtorres/Data/Dropbox/IST/tokamakstraining/ISTTOK
 atom .
 gnome-terminal .
 nautilus .
-
